ea85be1ec9e900a53751811108b4fdde05567298
Marco La Rosa <m@lr.id.au>
2018-07-19 15:22:11 +1000

    update data-service and associated tests

--- src/services/data-service.spec.js
+++ src/services/data-service.spec.js
@@ -1,12 +1,12 @@
-'use strict';
+"use strict";
 
-const chai = require('chai');
-chai.use(require('chai-json-schema'));
+const chai = require("chai");
+chai.use(require("chai-json-schema"));
 const expect = chai.expect;
-const {each} = require('lodash');
-const fs = require('fs');
-const shell = require('shelljs');
-const schema = require('./schema/item-data.schema');
+const { each } = require("lodash");
+const fs = require("fs");
+const shell = require("shelljs");
+const schema = require("./schema/item-data.schema");
 const {
     buildDataTree,
     readCatalogFile,
@@ -15,68 +15,68 @@ const {
     installTheData,
     writeIndexFile,
     updateLibraryBoxConfigurationFiles
-} = require('./data-service');
-const installationTargetFolder = '/tmp/LB/LibraryBox';
-const collectionViewer = './src/viewer';
+} = require("./data-service");
+const installationTargetFolder = "/tmp/LB/LibraryBox";
+const collectionViewer = "./src/viewer";
 
 if (!process.env.DATA_PATH) {
-    console.log('Please set DATA_PATH in the environment. It needs to');
-    console.log('  point to the location of the datafiles to be loaded.');
+    console.log("Please set DATA_PATH in the environment. It needs to");
+    console.log("  point to the location of the datafiles to be loaded.");
     process.exit();
 }
 
-if (!process.env.LIBRARY_BOX_MOUNTPOINT) {
-    console.log('Please set LIBRARY_BOX_MOUNTPOINT in the environment. It ');
-    console.log('needs to point to the mountpoint of the LibraryBox USB disk.');
+if (!process.env.LIBRARYBOX_MOUNTPOINT) {
+    console.log("Please set LIBRARYBOX_MOUNTPOINT in the environment. It ");
+    console.log("needs to point to the mountpoint of the LibraryBox USB disk.");
     process.exit();
 }
 
-describe('test data service methods', () => {
+describe("test data service methods", () => {
     afterEach(() => {
         shell.exec(`rm -rf ${installationTargetFolder}`);
     });
-    it('should be able to build a tree of data files to load', async () => {
-        const {items, errors} = await buildDataTree(process.env.DATA_PATH);
+    it("should be able to build a tree of data files to load", async () => {
+        const { items, errors } = await buildDataTree(process.env.DATA_PATH);
         // console.log(items, errors);
-        expect(items).to.be.an('array');
+        expect(items).to.be.an("array");
         items.forEach(item => {
             expect(item).to.have.keys(
-                'collectionId',
-                'itemId',
-                'dataPath',
-                'dataFile'
+                "collectionId",
+                "itemId",
+                "dataPath",
+                "dataFile"
             );
         });
     });
 
-    it('should be able to read and convert a datafile to JSON', async () => {
-        const {items, errors} = await buildDataTree(process.env.DATA_PATH);
+    it("should be able to read and convert a datafile to JSON", async () => {
+        const { items, errors } = await buildDataTree(process.env.DATA_PATH);
         const data = readCatalogFile(items[0]);
         expect(data).to.be.jsonSchema(schema);
     });
 
-    it('should be able to create an index file with all of the data', async () => {
-        const {items, errors} = await buildDataTree(process.env.DATA_PATH);
+    it("should be able to create an index file with all of the data", async () => {
+        const { items, errors } = await buildDataTree(process.env.DATA_PATH);
         const index = buildIndex(items);
-        expect(index).to.be.an('array');
+        expect(index).to.be.an("array");
         each(index, item => {
             expect(item.data).to.be.jsonSchema(schema);
         });
     });
 
-    it('should be able to wipe a library box (fake) target', () => {
-        shell.mkdir('-p', installationTargetFolder);
+    it("should be able to wipe a library box (fake) target", () => {
+        shell.mkdir("-p", installationTargetFolder);
         prepareTarget(installationTargetFolder);
         expect(statDirectory(`${installationTargetFolder}/www`)).to.be.true;
         expect(statDirectory(`${installationTargetFolder}/www/repository`)).to
             .be.true;
     });
 
-    it('should be able to install the collection viewer', () => {
-        shell.mkdir('-p', installationTargetFolder);
+    it("should be able to install the collection viewer", () => {
+        shell.mkdir("-p", installationTargetFolder);
         prepareTarget(installationTargetFolder);
         shell.cp(
-            '-r',
+            "-r",
             `${collectionViewer}/*`,
             `${installationTargetFolder}/www/`
         );
@@ -84,16 +84,23 @@ describe('test data service methods', () => {
         statFile(file);
     });
 
-    it('should be able to install the data and write the index file', async () => {
-        shell.mkdir('-p', installationTargetFolder);
+    it("should be able to install the data and write the index file", async () => {
+        shell.mkdir("-p", installationTargetFolder);
         prepareTarget(installationTargetFolder);
-        let {items, errors} = await buildDataTree(process.env.DATA_PATH);
+        let { items, errors } = await buildDataTree(process.env.DATA_PATH);
         let index = buildIndex(items);
-        const result = installTheData(
-            process.env.DATA_PATH,
-            installationTargetFolder,
-            index
-        );
+        // console.log(index);
+        const loggers = {
+            logInfo: () => {},
+            logError: () => {},
+            logComplete: () => {}
+        };
+        const result = installTheData({
+            dataPath: process.env.DATA_PATH,
+            target: installationTargetFolder,
+            index,
+            loggers
+        });
         index = result.index;
         each(index, item => {
             item.data.images.map(
@@ -125,16 +132,16 @@ describe('test data service methods', () => {
         ).to.be.true;
     }).timeout(10000);
 
-    it.only('should be able configure the defaults', () => {
+    it("should be able to configure the defaults", () => {
         expect(
             statDirectory(
-                `${process.env.LIBRARY_BOX_MOUNTPOINT}/LibraryBox/Config`
+                `${process.env.LIBRARYBOX_MOUNTPOINT}/LibraryBox/Config`
             )
         ).to.be.true;
         updateLibraryBoxConfigurationFiles({
-            target: process.env.LIBRARY_BOX_MOUNTPOINT,
-            hostname: 'catalog.paradisec.offline',
-            ssid: 'PARADISEC Catalog'
+            target: process.env.LIBRARYBOX_MOUNTPOINT,
+            hostname: "catalog.paradisec.offline",
+            ssid: "PARADISEC Catalog"
         });
     });
 });
--- src/services/librarybox-mgr-service.spec.js
+++ src/services/librarybox-mgr-service.spec.js
@@ -1,28 +1,28 @@
-'use strict';
+"use strict";
 
-const chai = require('chai');
-chai.use(require('chai-json-schema'));
+const chai = require("chai");
+chai.use(require("chai-json-schema"));
 const expect = chai.expect;
-const {each} = require('lodash');
+const { each } = require("lodash");
 const {
     checkTelnetAccessible,
     setDeviceRootPassword,
     canLoginOverSSH,
     reconfigureLibraryBox
-} = require('./librarybox-mgr-service');
+} = require("./librarybox-mgr-service");
 
 if (!process.env.LIBRARYBOX_IP) {
-    console.log('Please set LIBRARYBOX_IP in the environment. It needs to');
-    console.log('  be set to the IP address of the librarybox device.');
+    console.log("Please set LIBRARYBOX_IP in the environment. It needs to");
+    console.log("  be set to the IP address of the librarybox device.");
     process.exit();
 }
 
-describe('test configuring a librarybox', () => {
+describe.skip("test configuring a librarybox", () => {
     afterEach(() => {});
-    it('should determine whether telnet is accessible', async () => {
+    it("should determine whether telnet is accessible", async () => {
         try {
             let response = await checkTelnetAccessible({
-                deviceIpAddress: '192.168.1.1'
+                deviceIpAddress: "192.168.1.1"
             });
             expect(response).to.be.true;
         } catch (error) {
@@ -32,34 +32,34 @@ describe('test configuring a librarybox', () => {
     it(`should be able to set the device password over telnet or know it's been done`, async () => {
         try {
             let response = await setDeviceRootPassword({
-                rootPassword: 'paradisec',
-                deviceIpAddress: '192.168.1.1'
+                rootPassword: "paradisec",
+                deviceIpAddress: "192.168.1.1"
             });
             expect(response).to.equal(
-                'Root password set on device. Use SSH to login.'
+                "Root password set on device. Use SSH to login."
             );
         } catch (error) {
             expect(error.message).to.equal(
-                'Telnet disabled on device. Use SSH.'
+                "Telnet disabled on device. Use SSH."
             );
         }
     });
-    it('should be able to log in over SSH', async () => {
+    it("should be able to log in over SSH", async () => {
         try {
             let response = await canLoginOverSSH({
-                rootPassword: 'paradisec',
-                deviceIpAddress: '192.168.1.1'
+                rootPassword: "paradisec",
+                deviceIpAddress: "192.168.1.1"
             });
             expect(response).to.be.true;
         } catch (e) {
             expect(response).to.be.false;
         }
     }).timeout(5000);
-    it('should be able to reconfigure a librarybox', async () => {
+    it("should be able to reconfigure a librarybox", async () => {
         try {
             await reconfigureLibraryBox({
-                rootPassword: 'paradisec',
-                deviceIpAddress: '192.168.1.1'
+                rootPassword: "paradisec",
+                deviceIpAddress: "192.168.1.1"
             });
             // console.log(response);
         } catch (e) {
